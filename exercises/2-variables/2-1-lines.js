57d845199e464ace3b32ad7c9a075cd9179fa57b
remygist <remy.gistelinck@student.ehb.be>
2022-10-12 14:05:26 +0200

    2-1 Created some lines

---
+++ exercises/2-variables/2-1-lines.js
@@ -0,0 +1,31 @@
+"use strict";
+
+let canvas = document.querySelector('canvas');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+let context = canvas.getContext('2d');
+
+let width = context.canvas.width;
+let margin = 150;
+
+drawLines();
+
+
+
+function drawLines(){
+    context.beginPath();
+    context.moveTo(margin, 50);
+    context.lineTo(width - margin, 50);
+    context.lineTo(margin, 150);
+    context.lineTo(width - margin, 150);
+    context.lineTo(margin, 250);
+    context.lineTo(width - margin, 250);
+    context.lineTo(margin, 350);
+    context.lineTo(width - margin, 350);
+    context.lineTo(margin, 450);
+    context.lineTo(width - margin, 450);
+    context.lineTo(margin, 550);
+    context.lineTo(width - margin, 550);
+    context.lineTo(margin, 50);
+    context.stroke();
+}
