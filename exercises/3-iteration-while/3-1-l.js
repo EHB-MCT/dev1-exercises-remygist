11d4aad06e8116e6b4e6a1a20def584fe73228ce
remygist <remy.gistelinck@student.ehb.be>
2022-10-19 16:11:53 +0200

    3-1-l

---
+++ exercises/3-iteration-while/3-1-l.js
@@ -0,0 +1,22 @@
+"use strict";
+import context from "../../scripts/context.js";
+import * as Utils from "../../scripts/utils.js";
+
+draw();
+
+function draw() {
+    context.lineWidth = 2;
+    context.strokeStyle = "white";
+    context.fillStyle = "#3498DB";
+    context.fillRect(50, 50, 300, 300);
+    drawHourGlass();
+}
+
+function drawHourGlass() {
+    let i = 0;
+    let margin = 10;
+    while (i <= 30) {
+        Utils.drawLine(50 + (margin * i), 50 , 350 - (margin * i), 350);
+        i++;
+    }
+}
