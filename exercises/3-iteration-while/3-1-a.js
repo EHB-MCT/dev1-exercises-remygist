e0726357fa36267bbe2f34dc4252167e783a6ca3
remygist <remy.gistelinck@student.ehb.be>
2022-10-19 14:37:14 +0200

    3-1-a created

---
+++ exercises/3-iteration-while/3-1-a.js
@@ -0,0 +1,26 @@
+"use strict";
+import context from "../../scripts/context.js";
+import * as Utils from "../../scripts/utils.js";
+
+draw();
+
+function draw() {
+    context.lineWidth = 2;
+    context.strokeStyle = "white";
+    context.fillStyle = "#3498DB";
+    context.fillRect(50, 50, 300, 300);
+    drawLines();
+}
+
+function drawLines() {
+    let i = 0;
+    let margin = 25;
+    while (i < 11) {
+        //This drawLine function does not exist yet!
+        //Add it to the utils script:
+        //The function draws a line between 2 points
+        //And has the following parameters: x1, y1, x2, y2
+        Utils.drawLine(75, 75 + (margin * i), 325 , 75 + (margin * i));
+        i++;
+    }
+}
