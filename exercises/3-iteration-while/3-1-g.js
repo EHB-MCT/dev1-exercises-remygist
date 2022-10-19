be130d0651ffc4e0596cdde208143b0eeeeab1b0
remygist <remy.gistelinck@student.ehb.be>
2022-10-19 15:46:00 +0200

    3-1-g

---
+++ exercises/3-iteration-while/3-1-g.js
@@ -0,0 +1,28 @@
+"use strict";
+import context from "../../scripts/context.js";
+import * as Utils from "../../scripts/utils.js";
+
+draw();
+
+function draw() {
+    context.lineWidth = 2;
+    context.strokeStyle = "white";
+    context.fillStyle = "#3498DB";
+    context.fillRect(50, 50, 300, 300);
+    drawConcentricCircles();
+    console.log(Utils);
+}
+
+function drawConcentricCircles() {
+    let i = 0;
+    let margin = 15
+    while (i < 10) {
+        //This strokeCircle function does not exist yet!
+        //Add it to the utils script:
+        //The function draws the stroke of a circle 
+        //and has the following parameters: x,y,radius
+        Utils.strokeCircle(200,200,15+(margin*i));
+        console.log(i);
+        i++;
+    }
+}
