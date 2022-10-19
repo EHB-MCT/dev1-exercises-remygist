a4822f0a89ea152051361881e8bac338b0d26ac2
remygist <remy.gistelinck@student.ehb.be>
2022-10-19 14:44:45 +0200

    3-1-b

---
+++ exercises/3-iteration-while/3-1-b.js
@@ -0,0 +1,25 @@
+"use strict";
+import context from "../../scripts/context.js";
+import * as Utils from "../../scripts/utils.js";
+
+draw();
+
+function draw() {
+    context.lineWidth = 2;
+    context.strokeStyle = "white";
+    context.fillStyle = "#3498DB";
+    context.fillRect(50, 50, 300, 300);
+    drawGrid();
+}
+
+function drawGrid() {
+    let i = 0;
+    let margin = 50;
+    while (i < 6) {
+        let x = 75 + (margin * i);
+        let y = x;
+        Utils.drawLine(75, y, 325, y);
+        Utils.drawLine(x, 75, x, 325);
+        i++;
+    }
+}
