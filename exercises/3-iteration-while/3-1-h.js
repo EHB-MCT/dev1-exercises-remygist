6485e507b456301bdf8ac1ea5cdb5ae5f07fcc43
remygist <remy.gistelinck@student.ehb.be>
2022-10-19 15:53:09 +0200

    3-1-h

---
+++ exercises/3-iteration-while/3-1-h.js
@@ -0,0 +1,23 @@
+"use strict";
+import context from "../../scripts/context.js";
+import * as Utils from "../../scripts/utils.js";
+
+draw();
+
+function draw() {
+    context.lineWidth = 2;
+    context.strokeStyle = "white";
+    context.fillStyle = "#3498DB";
+    context.fillRect(50, 50, 300, 300);
+    drawMoreConcentricCircles();
+}
+
+function drawMoreConcentricCircles() {
+    context.lineWidth = 2;
+    let i = 13;
+    let margin = 15;
+    while (i > 0) {
+        Utils.strokeCircle(200, 200, 0 + (margin * i) );
+        i--;
+    }
+}
