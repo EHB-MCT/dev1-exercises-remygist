42d8e9e8d21c9355ef49ffd603689b2742d212ea
remygist <remy.gistelinck@student.ehb.be>
2022-10-26 16:23:48 +0200

    Created 4-3

--- exercises/4-iteration-for/4-3-Brick.js
+++ exercises/4-iteration-for/4-3-Brick.js
@@ -7,14 +7,12 @@ let height = context.canvas.height;
 let x = 50;
 let y = 50;
 
-
-
 for (let i = 0; i < 6; i++) {
     for (let j = 0; j < 3; j++) {
-        context.fillStyle = "#8b0000"
+        context.fillStyle = "#8b0000";
         Utils.FillRectangle(x + (160 * i), y + (120 * j), 150, 50);
         for (let k = 0; k < 3; k++) {
-            context.fillStyle = "#8b0000"
+            context.fillStyle = "#8b0000";
             Utils.FillRectangle((x + 50) + (160 * i), (y + 60) + (120 * j), 150, 50);
         }
     }
