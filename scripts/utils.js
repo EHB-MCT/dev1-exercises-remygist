2a24631353ab7e3a58846672dc7fec042caf2e04
remygist <remy.gistelinck@student.ehb.be>
2022-10-26 14:00:15 +0200

    new scripts

--- scripts/utils.js
+++ scripts/utils.js
@@ -8,3 +8,14 @@ export function drawLine(x1,y1,x2,y2){
     context.lineTo(x2,y2);
     context.stroke();
 }
+
+export function strokeCircle(x,y,radius){
+    context.beginPath();
+    context.ellipse(x,y,radius,radius,0,0,2*Math.PI,false);
+    context.stroke();
+}
+
+export function rgb(r, g, b){
+    let rgb = "rgb(" + r + "," + g + "," + b + ")";
+    return rgb;
+}
