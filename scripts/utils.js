2ccdabce6f00f12aa45a83258f6135840f44db5d
remygist <remy.gistelinck@student.ehb.be>
2022-10-26 15:01:14 +0200

    random function

--- scripts/utils.js
+++ scripts/utils.js
@@ -15,7 +15,22 @@ export function strokeCircle(x,y,radius){
     context.stroke();
 }
 
+export function fillCircle(x,y,radius){
+    context.beginPath();
+    context.ellipse(x,y,radius,radius,0,0,2*Math.PI,false);
+    context.fill();
+}
+
 export function rgb(r, g, b){
     let rgb = "rgb(" + r + "," + g + "," + b + ")";
     return rgb;
+}
+
+export function rgba(r, g, b, a){
+    let rgba = "rgba(" + r + "," + g + "," + b + "," + a + ")";
+    return rgba;
+}
+
+export function randomInt(min, max){
+    return Math.floor(Math.random() * (max - min + 1) + min);
 }
