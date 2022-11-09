b186d3443ca88fb7db66139e98b46983575eb2e7
remygist <remy.gistelinck@student.ehb.be>
2022-11-09 16:06:27 +0100

    Created 5-2

---
+++ exercises/5-selection/5-2-a-maze.js
@@ -0,0 +1,22 @@
+"use strict";
+import context from "../../scripts/context.js";
+import * as Utils from "../../scripts/utils.js";
+
+let width = context.canvas.width;
+let height = context.canvas.height;
+
+drawMaze();
+
+function drawMaze() {
+	context.lineWidth = 5;
+    context.lineCap = "square";
+	for (let j = 0; j < height / 10; j++) {
+		for (let i = 0; i < width / 10; i++) {
+			if (Math.round(Math.random()) == 1) {
+				Utils.drawLine(0 + i * 10, 0 + j * 10, 10 + i * 10, 10 + j * 10);
+			} else {
+				Utils.drawLine(10 + i * 10, 0 + j * 10, 0 + i * 10, 10 + j * 10);
+			}
+		}
+	}
+}
